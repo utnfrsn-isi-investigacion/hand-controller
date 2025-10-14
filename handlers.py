3189037701c3b8015181a0af1ffb5010ad1745b0
Alejandro Recalde <alejandrorecalde5@gmail.com>
2025-10-14 20:48:11 -0300

    refactor: Make process_hands an abstract method and move its implementation to the concrete class

--- handlers.py
+++ handlers.py
@@ -14,32 +14,12 @@ class Handler(abc.ABC):
             HandType.RIGHT: None,
         }
 
+    @abc.abstractmethod
     def process_hands(self, hands: List[Hand]) -> None:
         """Process a list of detected hands and send actions."""
+        pass
 
-        # Create a dictionary of hand types present in the current frame
-        detected_hands_map = {hand.get_hand_type(): hand for hand in hands if hand.get_hand_type() != HandType.UNKNOWN}
-
-        # Determine and send action for the left hand (throttle)
-        if HandType.LEFT in detected_hands_map:
-            left_action = self.get_action(detected_hands_map[HandType.LEFT])
-        else:
-            left_action = CarHandler.Action.STOP
-
-        if self._last_actions.get(HandType.LEFT) != left_action:
-            self._send_action(HandType.LEFT, left_action)
-
-        # Determine and send action for the right hand (direction)
-        if HandType.RIGHT in detected_hands_map:
-            right_action = self.get_action(detected_hands_map[HandType.RIGHT])
-        else:
-            right_action = CarHandler.Action.DIRECTION_STRAIGHT
-
-        if self._last_actions.get(HandType.RIGHT) != right_action:
-            self._send_action(HandType.RIGHT, right_action)
-
-
-    def _send_action(self, hand_type: HandType, action: 'CarHandler.Action'):
+    def _send_action(self, hand_type: HandType, action: Enum):
         """Sends the action and updates the last action for the given hand type."""
         if self._esp32_connector.is_connected():
             self._esp32_connector.send_action(action.value)
@@ -51,12 +31,36 @@ class Handler(abc.ABC):
 
 
 class CarHandler(Handler):
-    class Action(str, Enum):
+    class Action(Enum):
         ACCELERATE = "001"
         STOP = "000"
         DIRECTION_LEFT = "101"
         DIRECTION_RIGHT = "110"
         DIRECTION_STRAIGHT = "111"
+
+    def process_hands(self, hands: List[Hand]) -> None:
+        """Process a list of detected hands and send actions for car control."""
+
+        # Create a dictionary of hand types present in the current frame
+        detected_hands_map = {hand.get_hand_type(): hand for hand in hands if hand.get_hand_type() != HandType.UNKNOWN}
+
+        # Determine and send action for the left hand (throttle)
+        if HandType.LEFT in detected_hands_map:
+            left_action = self.get_action(detected_hands_map[HandType.LEFT])
+        else:
+            left_action = self.Action.STOP
+
+        if self._last_actions.get(HandType.LEFT) != left_action:
+            self._send_action(HandType.LEFT, left_action)
+
+        # Determine and send action for the right hand (direction)
+        if HandType.RIGHT in detected_hands_map:
+            right_action = self.get_action(detected_hands_map[HandType.RIGHT])
+        else:
+            right_action = self.Action.DIRECTION_STRAIGHT
+
+        if self._last_actions.get(HandType.RIGHT) != right_action:
+            self._send_action(HandType.RIGHT, right_action)
 
     def get_action(self, hand: Hand) -> Action:
         """Return the Action for this hand based on type and gesture."""
